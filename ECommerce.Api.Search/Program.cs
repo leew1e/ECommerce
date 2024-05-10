553f66c313dd8d9c8179b67311825519a92c524e
leew1e <ostrovdmytriy@gmail.com>
2024-05-10 20:14:46 +0300

    Finish 3d part

--- ECommerce.Api.Search/Program.cs
+++ ECommerce.Api.Search/Program.cs
@@ -1,6 +1,23 @@
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// -------------------------------------------
+builder.Services.AddScoped<ISearchService, SearchService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+builder.Services.AddScoped<IProductsService, ProductsService>();
+builder.Services.AddHttpClient("OrdersService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Orders"]);
+});
+builder.Services.AddHttpClient("ProductsService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Products"]);
+});
+
+// -------------------------------------------
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
