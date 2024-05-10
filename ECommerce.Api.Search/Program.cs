bfd1d925382bde1a332aa9d133f7d629b6dbfc43
leew1e <ostrovdmytriy@gmail.com>
2024-05-10 20:53:24 +0300

    Finish 5th

--- ECommerce.Api.Search/Program.cs
+++ ECommerce.Api.Search/Program.cs
@@ -1,13 +1,19 @@
 using ECommerce.Api.Search.Interfaces;
 using ECommerce.Api.Search.Services;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // -------------------------------------------
 builder.Services.AddScoped<ISearchService, SearchService>();
+builder.Services.AddScoped<ICustomersService, CustomersService>();
 builder.Services.AddScoped<IOrdersService, OrdersService>();
 builder.Services.AddScoped<IProductsService, ProductsService>();
+builder.Services.AddHttpClient("CustomersService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Customers"]);
+});
 builder.Services.AddHttpClient("OrdersService", config =>
 {
     config.BaseAddress = new Uri(builder.Configuration["Services:Orders"]);
@@ -15,7 +21,7 @@ builder.Services.AddHttpClient("OrdersService", config =>
 builder.Services.AddHttpClient("ProductsService", config =>
 {
     config.BaseAddress = new Uri(builder.Configuration["Services:Products"]);
-});
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
 
 // -------------------------------------------
 
--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -4,29 +4,42 @@ namespace ECommerce.Api.Search.Services
 {
     public class SearchService : ISearchService
     {
-        private readonly IOrdersService ordersService;
         private readonly IProductsService productsService;
-        public SearchService(IOrdersService ordersService, IProductsService productsService)
+        private readonly IOrdersService ordersService;
+        private readonly ICustomersService customersService;
+        public SearchService(IProductsService productsService, IOrdersService ordersService, ICustomersService customersService)
         {
-            this.ordersService = ordersService;
             this.productsService = productsService;
+            this.ordersService = ordersService;
+            this.customersService = customersService;
         }
-
-        public async Task<(bool IsSuccess, dynamic SearchResults)>SearchAsync(int customerId)
+        public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
+            var customersResult = await customersService.GetCustomerAsync(customerId);
             var ordersResult = await ordersService.GetOrdersAsync(customerId);
             var productsResult = await productsService.GetProductsAsync();
+
             if (ordersResult.IsSuccess)
             {
-                foreach (var order in ordersResult.Orders)
+                foreach (var orders in ordersResult.Orders)
                 {
-                    foreach (var item in order.Items)
+                    foreach (var item in orders.Items)
                     {
-                        item.ProductName = productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name;
+                        item.ProductName = productsResult.IsSuccess
+                        ?
+                        productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name
+                        :
+                        "Product information is not available";
                     }
                 }
                 var result = new
                 {
+                    Customer = customersResult.IsSuccess ?
+                                customersResult.Customer :
+                                new
+                                {
+                                    Name = "Customer information is not available",
+                                },
                     Orders = ordersResult.Orders
                 };
                 return (true, result);
